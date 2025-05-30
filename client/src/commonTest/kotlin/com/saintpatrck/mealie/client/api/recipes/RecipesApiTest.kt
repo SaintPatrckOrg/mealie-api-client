f1f08ad2eb4312431df3283cd851cf7d67bb38cb
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2025-05-29 22:40:26 -0400

    Add `RecipesApi` and `testScrapeUrl` endpoint (#52)
    This commit introduces the `RecipesApi` with its first endpoint,
    `testScrapeUrl`.
    
    It includes:
    - `RecipesApi` interface for recipe-related operations.
    - `testScrapeUrl` function to test scraping a URL for recipe
    information.
    - `TestScrapeUrlRequestJson` data class for the request body of
    `testScrapeUrl`.
    - `TestScrapeUrlResponseJson` data class for the response body of
    `testScrapeUrl`, including a nested `RecipeInstruction` class.
    - Corresponding test cases in `RecipesApiTest` to ensure correct
    deserialization.
    - Integration of `RecipesApi` into the `MealieClient`.

---
+++ client/src/commonTest/kotlin/com/saintpatrck/mealie/client/api/recipes/RecipesApiTest.kt
@@ -0,0 +1,82 @@
+package com.saintpatrck.mealie.client.api.recipes
+
+import com.saintpatrck.mealie.client.api.base.BaseApiTest
+import com.saintpatrck.mealie.client.api.model.getOrThrow
+import com.saintpatrck.mealie.client.api.recipes.model.TestScrapeUrlRequestJson
+import com.saintpatrck.mealie.client.api.recipes.model.TestScrapeUrlResponseJson
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RecipesApiTest : BaseApiTest() {
+
+    @Test
+    fun `testScrapeUrl should deserialize correctly`() = runTest {
+        createTestMealieClient(
+            responseJson = RECIPE_JSON,
+        )
+            .recipesApi
+            .testScrapeUrl(
+                testScrapeUrlRequest = TestScrapeUrlRequestJson(
+                    url = "mockUrl",
+                    useOpenAi = false,
+                )
+            )
+            .also { response ->
+                assertEquals(
+                    createMockRecipeJson(),
+                    response.getOrThrow(),
+                )
+            }
+    }
+}
+
+private const val RECIPE_JSON = """
+{
+    "@context": "https://schema.org/",
+    "@type": "Recipe",
+    "name": "BALTIMORE BLONDE FRIED CHICKEN",
+    "image": [
+        "https://images.ctfassets.net/8nq3bs98o7cv/6m5b87dnExetfdzn4dvPgX/8c694e17478abd939cc2219a312fd9a1/Baltimore_Blonde_Fried_Chicken"
+    ],
+    "description": "This recipe shows how Guinness can unlock unique flavours and new possibilities while cooking at home.",
+    "prepTime": "PT2 hours 45 mins",
+    "cookTime": "PT15 mins",
+    "totalTime": "PT2 hours 45 minsPT15 mins",
+    "recipeYield": "2-3",
+    "author": "Guinness Rebuild",
+    "recipeCategory": "Main",
+    "recipeIngredient": [
+        "BEER BRINE"
+    ],
+    "recipeInstructions": [
+        {
+            "@type": "HowToStep",
+            "text": "BEER BRINE"
+        }
+    ]
+}
+"""
+
+private fun createMockRecipeJson() = TestScrapeUrlResponseJson(
+    context = "https://schema.org/",
+    type = "Recipe",
+    name = "BALTIMORE BLONDE FRIED CHICKEN",
+    image = listOf("https://images.ctfassets.net/8nq3bs98o7cv/6m5b87dnExetfdzn4dvPgX/8c694e17478abd939cc2219a312fd9a1/Baltimore_Blonde_Fried_Chicken"),
+    description = "This recipe shows how Guinness can unlock unique flavours and new possibilities while cooking at home.",
+    prepTime = "PT2 hours 45 mins",
+    cookTime = "PT15 mins",
+    totalTime = "PT2 hours 45 minsPT15 mins",
+    recipeYield = "2-3",
+    author = "Guinness Rebuild",
+    recipeCategory = "Main",
+    recipeIngredient = listOf(
+        "BEER BRINE",
+    ),
+    recipeInstructions = listOf(
+        TestScrapeUrlResponseJson.RecipeInstruction(
+            type = "HowToStep",
+            text = "BEER BRINE"
+        ),
+    ),
+)
